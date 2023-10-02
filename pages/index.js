f40479a0a45029c844ea67c302c96a9b5c54988b
Satyam singh <satyamsingh2003a@gmail.com>
2023-10-02 10:13:16 +0530

    hi

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,86 @@
-import Head from 'next/head';
+import { useState } from 'react'; // Import the useState hook
+
 import styles from '../styles/Home.module.css';
 
+
 export default function Home() {
+  // Define state variables for red, green, and blue values
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
+  const selectedColor = `Selected RGB Color: rgb(${red}, ${green}, ${blue})`;
+
+  // Define functions to update the color values
+  const updateRed = (e) => {
+    setRed(e.target.value);
+  };
+
+  const updateGreen = (e) => {
+    setGreen(e.target.value);
+  };
+
+  const updateBlue = (e) => {
+    setBlue(e.target.value);
+  };
+
+  // Define the styles object for the colored box
+  const boxStyles = {
+    marginTop: 100,
+    height: 500,
+    width: 500,
+    background: `rgb(${red}, ${green}, ${blue})`,
+  };
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+   
+    
+    <div className="container">
+      <div className="sliders">
+        <div className="red">
+          <input
+            id="red"
+            type="range"
+            min="0"
+            max="255"
+            step="1"
+            value={red}
+            onChange={updateRed}
+          
+          />
+          <label>Red: {red}</label>
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+        <div className="green">
+          <input
+            id="green"
+            type="range"
+            min="0"
+            max="255"
+            step="1"
+            value={green}
+            onChange={updateGreen}
+          />
+          <label>Green: {green}</label>
+        </div>
+        <div className="blue">
+          <input
+            id="blue"
+            type="range"
+            min="0"
+            max="255"
+            step="1"
+            value={blue}
+            onChange={updateBlue}
+          />
+          <label>Blue: {blue}</label>
+        </div>
+      </div>
+      {/* Display the colored box */}
+      <div className="colored-box" style={boxStyles}></div>
+      <div>
+        <p>{selectedColor}</p>
+      </div>
     </div>
   );
 }
